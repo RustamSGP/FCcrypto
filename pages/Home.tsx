0c00e3f55c0388376b83bcdf7ef7478c66c0809d
RustamSGP <sgpgamesnft@gmail.com>
2023-03-28 19:08:37 +0300

    Site with img

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,14 +1,53 @@
 import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import {
+  MediaRenderer,
+  useActiveListings,
+  useContract,
+} from "@thirdweb-dev/react";
+import { useRouter } from "next/router";
+import { marketplaceContractAddress } from "../addressesM";
 
 
 const Home: NextPage = () => {
- 
-
-  return (
-   <div> 
-    <h1>juy </h1> 
-     </div>
-  );
-};
-
+  const router = useRouter();
+  const { contract: marketplace } = useContract(marketplaceContractAddress, "marketplace");
+  const { data: listings, isLoading: loadingListings } = useActiveListings(marketplace);
+    return (
+          <div>
+            <div className="Main"> 
+              <h1>Клубiв багато 
+                <a>                - найкращий один! </a></h1>   
+            </div>
+    
+            <div className="Content">
+              <div className="Grid">
+                <div className="grid1">
+                  <div className="section1"> Мы открыли свой crypto FanShop</div>
+                  <div className="section2"></div>
+                </div>
+    
+                <div className="grid2">
+                  <div className="section3"></div>
+                  <div className="section4"></div>
+                </div>
+              </div>
+    
+              <div className="grid3">
+                <div className="section5"></div>
+                <div className="section5"></div>
+                <div className="section5"></div>
+                <div className="section5"></div>
+              </div>
+            </div>
+    
+            <div className="footer">
+              <h1>Клубiв багато 
+                <a>             - найкращий один! </a></h1>   
+            </div>
+          </div>
+      );
+    };
+    
 export default Home;
